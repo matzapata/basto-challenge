1cffb146a4c17382f3c6838d0ce2157b08fda657
Matias Zapata <matuzapata@gmail.com>
2022-10-20 11:35:34 -0300

    Chakra UI setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Container, Heading } from "@chakra-ui/react";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container minW="container.lg" my="10">
+      <Heading>Gestion de animales</Heading>
+    </Container>
+  );
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./utils/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
